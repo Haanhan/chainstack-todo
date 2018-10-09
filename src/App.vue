e16b4d13b8f01644123064f9f283902cd156c364
Lee Shei Pin <shei.pin@toccostudios.com>
2018-10-09 18:45:27 +0800

    add bulma ui, first ui commit.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,72 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <section class="section">
+          <h3 class="title is-3 level-left">To-do List</h3>
+          <div class="field has-addons">
+              <div class="control is-expanded">
+                  <input class="input is-large" placeholder="Add a new item to do" type="text"/>
+              </div>
+              <div class="control">
+                  <button class="button is-primary is-large"
+                      onClick={addTodo} >
+                      <span class="fa fa-plus"></span>
+                  </button>
+              </div>
+          </div>
+
+          <div class="hero is-light">
+            <div class="hero-body has-text-centered">
+            <div class="stack-m fa fa-clipboard-list fa-3x"></div>
+              <h1 class="title is-4">You have nothing to do for now.</h1>
+              <p>Add to get started.</p>
+            </div>
+          </div>
+          <div class="notification is-info">
+              <span class="inline-m"><span class="far fa-lightbulb"></span></span> 
+              <span class="has-text-weight-bold inline-s">Tip:</span> Click on to-do item to edit.
+          </div>
+
+      
+      </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import "bulma/bulma.sass";
+
+//spacing
+$space-base: 20px;
+$stack-m: $space-base;
+$inline-m: $space-base;
+$inline-s: floor($inline-m / 2);
+
+.stack-m{
+  margin-bottom: $stack-m;
 }
+
+.inline-m{
+    margin-right: $inline-m;
+}
+
+.inline-s{
+    margin-right: $inline-s;
+}
+
+// .flex-1{
+//     flex:1;
+// }
+
+.cursor-pointer{
+    cursor:pointer;
+}
+
 </style>
