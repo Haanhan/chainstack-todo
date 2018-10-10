9697db92b9bb3b392f02ced9e59d487c7381e8d8
Lee Shei Pin <shei.pin@toccostudios.com>
2018-10-10 17:17:11 +0800

    Bring out Input Group markup as standalone component

---
+++ src/components/InputGroup.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="field has-addons">
+        <div class="control is-expanded">
+            <input class="input is-large" 
+                v-model="inputValue" 
+                :placeholder="placeholder" 
+                type="text"/>
+        </div>
+        <div class="control">
+            <button class="button is-primary is-large" @click="$emit('btnClick', inputValue)">
+                <slot></slot>
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default{
+        props:{
+            placeholder: {type: String},
+        },
+        data(){
+            return {
+                inputValue: ""
+            }
+        }
+    }
+</script>
