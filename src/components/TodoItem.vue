ec5cf72f659afe4d568224713c04995cafbdd2b9
Lee Shei Pin <shei.pin@toccostudios.com>
2018-10-10 18:17:47 +0800

    Add todo component

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,30 @@
+<template>
+    <a class="panel-block">
+
+        <span class="is-size-4 has-text-info inline-s cursor-pointer" >
+            <span class=""></span>
+        </span>
+
+        <span class="is-size-4 has-text-info inline-s cursor-pointer">
+            <span class=""></span>
+        </span>
+        <p class="flex-1">
+            {{text}}
+        </p>
+        
+        <button class="button is-text is-danger is-inverted">
+            <span class="icon">
+                <span class="fa fa-trash"></span>
+            </span>
+        </button>
+        
+    </a>
+</template>
+
+<script>
+    export default{
+        props:{
+            text: { type: String },
+        }
+    }
+</script>
---
+++ src/components/Todos.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="panel">
+        <template v-for="todo in todoList">
+            <TodoItem :text="todo.text" :key="todo.id"></TodoItem>
+        </template>
+    </div>
+</template>
+
+<script>
+import TodoItem from "./TodoItem.vue";
+import { mapState } from "vuex";
+
+    export default{
+        components: {
+            TodoItem
+        },
+        computed: {
+            ...mapState(["todoList"])
+        }
+    }
+</script>
