4dd6882c1209097d673d602dcc9b101713de25d4
Lee Shei Pin <shei.pin@toccostudios.com>
2018-10-10 23:59:53 +0800

    Add checkbox component

---
+++ src/components/Checkbox.vue
@@ -0,0 +1,33 @@
+<template>
+     <span class="is-size-4 has-text-primary inline-s cursor-pointer" @click="handleClick">
+            <span :class="icon"></span>
+    </span>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            value: false
+        }
+    },
+    props: {
+        isPending: { type:Boolean, default: false }
+    },
+    computed:{
+        icon(){
+            return this.isPending ?
+                "fa fa-spinner fa-spin" :
+            this.value ? 
+                "fa fa-check-circle" : 
+                "far fa-circle"
+        }
+    },
+    methods:{
+        handleClick(){
+            this.value = !this.value;
+            this.$emit("changed", this.value);
+        }
+    }
+}
+</script>
